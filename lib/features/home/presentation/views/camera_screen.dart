0b611885a17f8f8c718ced857a00e82ed652ec0c
Rewanelmlah <rwanmohamed86@icloud.com>
2025-02-18 15:53:52 +0200

    adding camera and prediction screens

--- lib/features/home/presentation/views/camera_screen.dart
+++ lib/features/home/presentation/views/camera_screen.dart
@@ -1,9 +1,134 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-class CameraScreen extends StatelessWidget {
-  const CameraScreen({super.key});
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:shamsoon/core/app_colors.dart';
+import 'package:shamsoon/features/home/presentation/views/prediction_screen.dart';
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _cameraController;
+  List<CameraDescription>? _cameras;
+  bool _isCameraInitialized = false;
+  XFile? _capturedImage;
+  final ImagePicker _picker = ImagePicker();
+
+  @override
+  void initState() {
+    super.initState();
+    initializeCamera();
+  }
+
+  void initializeCamera() async {
+    _cameras = await availableCameras();
+    _cameraController = CameraController(_cameras![0], ResolutionPreset.medium);
+    await _cameraController!.initialize();
+    setState(() {
+      _isCameraInitialized = true;
+    });
+  }
+
+  void captureImage() async {
+    if (_cameraController != null) {
+      XFile image = await _cameraController!.takePicture();
+      setState(() {
+        _capturedImage = image;
+      });
+    }
+  }
+
+  void pickImageFromGallery() async {
+    XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        _capturedImage = image;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _cameraController?.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Camera',
+          style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+        ),
+        leading: IconButton(
+          icon: Icon(Icons.close, size: 24.sp),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: _isCameraInitialized
+                ? SizedBox(
+                    width: double.infinity,  
+                    height: 0.4.sh,  
+                    child: Stack(
+                      children: [
+                        CameraPreview(_cameraController!),
+                        Center(
+                          child: Container(
+                            width: 0.50.sw,  // 45% of screen width
+                            height: 0.50.sw,  
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 3),
+                              borderRadius: BorderRadius.circular(10.r),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                : Center(child: CircularProgressIndicator()),
+          ),
+          Container(
+            color: Colors.white,
+            padding: EdgeInsets.all(20.w),  
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.photo_outlined, size: 50.sp, color: AppColors.primaryColor),
+                  onPressed: pickImageFromGallery,
+                ),
+                IconButton(
+                  icon: Icon(Icons.radio_button_checked_outlined, size: 60.sp, color: AppColors.primaryColor),
+                  onPressed: captureImage,
+                ),
+                IconButton(
+                  icon: Icon(Icons.check_circle, size: 50.sp, color: AppColors.primaryColor),
+                  onPressed: () {
+                    if (_capturedImage != null) {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => PredictionScreen(imagePath: _capturedImage!.path),
+                       ),
+                     );
+                    }
+                  },
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 30.h),  
+        ],
+      ),
+    );
   }
 }
---
+++ lib/features/home/presentation/views/prediction_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'dart:io';
+
+import 'package:shamsoon/core/app_colors.dart';
+
+class PredictionScreen extends StatelessWidget {
+  final String imagePath;
+
+  const PredictionScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, size: 22.sp),
+          onPressed: () => Navigator.pop(context),
+        ),
+        title: Text(
+          'Prediction',
+          style: TextStyle(fontSize: 20.sp, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10.w), 
+        child: Column(
+           
+          crossAxisAlignment: CrossAxisAlignment.center,
+          
+          children: [
+            SizedBox(height: 30.h), 
+            Container(
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white, width: 3.w),
+                borderRadius: BorderRadius.circular(25.r),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(25.r), 
+                child: Image.file(
+                  File(imagePath),
+                  height: 300.h, 
+                  width: 0.8.sw, 
+                  fit: BoxFit.cover, 
+                ),
+              ),
+            ),
+            SizedBox(height: 40.h),
+            Container(
+              padding: EdgeInsets.all(20.w), 
+              decoration: BoxDecoration(
+                border: Border.all(color:AppColors.primaryColor, width: 3.w),
+                borderRadius: BorderRadius.circular(25.r),
+              ),
+              child: RichText(
+                text: TextSpan(
+                  text: 'Your solar panel is: ',
+                  style: TextStyle(color: Colors.black, fontSize: 22.sp, fontWeight: FontWeight.bold),
+                  children: [
+                    TextSpan(
+                      text: 'dusty',
+                      style: TextStyle(fontSize: 22.sp, color: AppColors.primaryColor, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
