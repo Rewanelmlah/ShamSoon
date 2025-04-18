48eed4a3398a737bcc945ed0d3ffbcec623ae745
ahmed emara <emara8028@gmail.com>
2025-04-18 14:24:44 +0200

    change app name and put material attrebutes

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:shamsoon/core/shared/Functions/setup_service_locators.dart';
-import 'package:shamsoon/features/splash/splash_screen.dart';
-
+import 'core/helpers/constant_manager.dart';
+import 'core/helpers/loading_manager.dart';
+import 'core/helpers/navigation.dart';
+import 'core/shared/route_observer.dart';
+import 'core/widgets/offline_widget.dart';
 import 'firebase_options.dart';
 
 
@@ -14,13 +18,13 @@ void main() async {
   );
 
   setUpServiceLocator();
-  runApp(const MyApp());
+  runApp(const ShamSoon());
 }
 
-class MyApp extends StatelessWidget {
+class ShamSoon extends StatelessWidget {
   
 
-  const MyApp({super.key, });
+  const ShamSoon({super.key, });
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +34,18 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
-          theme: ThemeData.light(),
-          home: const SplashScreen(), // Start with
+          title: ConstantManager.projectName,
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          navigatorKey: Go.navigatorKey,
+          home: const Scaffold(),
+          navigatorObservers: [AppNavigationObserver()],
+          builder: (context, child) {
+            return OfflineWidget(
+              child: FullScreenLoadingManager(child: child!),
+            );
+          },
         );
       },
     );
